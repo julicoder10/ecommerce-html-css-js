cd6c8ee087da83c9902e445183f802f78f434ea8
Julicoder10 <julian.andres.aquino33@gmail.com>
2023-04-25 22:39:23 -0300

    modificaciones de diseño

--- script.js
+++ script.js
@@ -11,21 +11,6 @@ window.onscroll = () =>{
   navbar.classList.remove('active');
 }
 
-let slides = document.querySelectorAll('.slide-container');
-let index = 0;
-
-function next(){
-  slides[index].classList.remove('active');
-  index = (index + 1) % slides.length;
-  slides[index].classList.add('active');
-}
-
-function prev(){
-  slides[index].classList.remove('active');
-  index = (index - 1 + slides.length) % slides.length;
-  slides[index].classList.add('active');
-}
-
 document.querySelectorAll('.featured-image-1').forEach(image_1 =>{
   image_1.addEventListener('click', ()=>{
     var src = image_1.getAttribute('src');
